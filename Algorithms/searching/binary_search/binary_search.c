04bfd849eaf14f60540b16a18e5e2267545237bd
Abhishek Swain <abhiswain.it.2016@gmail.com>
2021-03-16 06:40:32 +0530

    fixed binary_search.c

--- Algorithms/searching/binary_search/binary_search.c
+++ Algorithms/searching/binary_search/binary_search.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+void insertionSort(int a[], int n)
+{
+    int key, j;
+    for (int i = 1; i < n; i++)
+    {
+        key = a[i];
+        j = i - 1;
+
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+}
+
 int binary_Search(int arr[], int l, int r, int key)
 {
     while (l <= r)
@@ -10,10 +29,10 @@ int binary_Search(int arr[], int l, int r, int key)
             return mid;
 
         else if (arr[mid] > key)
-            l = mid - 1;
+            r = mid - 1;
 
         else
-            r = mid + 1;
+            l = mid + 1;
     }
 
     return -1;
@@ -22,7 +41,10 @@ int binary_Search(int arr[], int l, int r, int key)
 int main()
 {
     int arr[] = {1, 3, 2, 0, 5, 4, -1};
-    int key = 3, n = sizeof(arr) / sizeof(arr[0]);
+    int key = 4, n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
     if (binary_Search(arr, 0, n - 1, key))
         printf("%d found at index %d\n", key, binary_Search(arr, 0, n - 1, key));
     else
